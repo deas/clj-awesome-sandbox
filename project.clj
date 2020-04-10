e832d9275d70ee74dfbf7b15d2b05aea12c06d7f
Andreas Steffan <a.steffan@contentreich.de>
2020-04-10 08:26:27 +0200

    added k8s-api

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring/ring-core "1.8.0"]
-                 [ring/ring-jetty-adapter "1.8.0"]]
+                 [ring/ring-jetty-adapter "1.8.0"]
+                 [nubank/k8s-api "0.1.0"]]
   :repl-options {:init-ns clj-awesome-sandbox.core})
