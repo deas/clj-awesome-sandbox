fc79c3039606904b5af2a6179b697dd4c590d630
Andreas Steffan <a.steffan@contentreich.de>
2020-04-11 15:20:36 +0200

    Token based auth ok

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring/ring-core "1.8.0"]
                  [ring/ring-jetty-adapter "1.8.0"]
+                 ;; [http-kit "2.4.0-alpha6"] ;; java 11
                  [nubank/k8s-api "0.1.0"]]
-  :repl-options {:init-ns clj-awesome-sandbox.test-core})
+  :repl-options {:init-ns clj-awesome-sandbox.core-test})
