57aee37d169ae6eb91930fd33a544e9298fbc58a
Andreas Steffan <a.steffan@contentreich.de>
2020-09-28 08:24:22 +0200

    wip: refactor

--- src/clj_awesome_sandbox/core.clj
+++
@@ -1,5 +0,0 @@
-(ns clj-awesome-sandbox.core
-  (:import (io.fabric8.kubernetes.client DefaultKubernetesClient)
-           (io.fabric8.kubernetes.api.model ConfigBuilder))
-  (:require [kubernetes-api.core :as k8s-api]))
-
