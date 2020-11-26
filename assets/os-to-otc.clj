5c9008d3c558fa171d0e1b562bda865953d37e0c
Andreas Steffan <a.steffan@contentreich.de>
2020-11-26 06:07:31 +0100

    fix: Massed otc tf state wrangling

--- assets/os-to-otc.clj
+++ assets/os-to-otc.clj
@@ -4,8 +4,8 @@
          '[clojure.java.io :as io])
 
 (defn tf-state-user-attributes
-  [stream]
-  (->> (json/parse-stream stream keyword)
+  [rdr]
+  (->> (json/parse-stream rdr keyword)
        :resources
        (filter #(= (:type %1) "openstack_identity_user_v3"))
        (map #(-> %1 :instances first :attributes))))
@@ -23,13 +23,18 @@
        dorun))
 
 (defn -main
-  "os-to-otc.clj 'terraform import openstack_identity_user_v3.%s %s' <state.json"
-  [[fmt] & _args]
+  "validate:
+   os-to-otc.clj 'terraform import openstack_identity_user_v3.%s %s' <state.json
+   
+   execute:
+   terraform state pull | os-to-otc.clj 'echo terraform import openstack_identity_user_v3.%s %s' | sh"
+  [[fmt] & _]
   (print-tf-users-fmt *in* fmt))
 
 (when (= *file* (System/getProperty "babashka.file"))
   (-main *command-line-args*))
 
+;; REPL
 (comment
  (-> (io/input-stream "state.json")
      io/reader
